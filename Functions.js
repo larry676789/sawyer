1c96530cc6ec0a97615d9e4bddec57368d6620ff
Hacker God <jyrone.parker@gmail.com>
2023-11-27 23:57:21 -0500

    added fibonnaci and factorial challenges

--- Functions.js
+++ Functions.js
@@ -5,6 +5,38 @@ function counter (count = 0) {
         count = count + 1 
     }
 }
+
+/**
+ * 
+ * Continuing our journey on functions and conditions write two functions, one called factorial that computes the factorial of n
+ * and another one called fibonnaci which returns the fibonnaci sequence of n
+ * 
+ * For a reference of what a fibonnaci sequence https://en.wikipedia.org/wiki/Fibonacci_sequence
+ * For a reference of what a factorial sequence https://en.wikipedia.org/wiki/Factorial
+ */
+
+
+/**
+ * 
+ * @param {integer} n - number to compute fibonnaci sequence of
+ * @returns {integer} fib - The fibonnaci sum 
+ */
+
+function fibonnaci(n) {
+
+}
+
+/**
+ * 
+ * @param {integer} n - number to computer factorial of
+ * @returns {integer} factorial - the factorial of n
+ */
+
+function factorial(n) {
+
+}
 module.exports = {
-    counter
+    counter,
+    fibonnaci,
+    factorial
 }

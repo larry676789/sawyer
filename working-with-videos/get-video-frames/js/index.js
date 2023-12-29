3c3d2a3b79d3391dded7c2f4b2a2b7c0f2d46edd
larry676789 <152258295+larry676789@users.noreply.github.com>
2023-12-29 03:34:05 +0000

    comments

---
+++ working-with-videos/get-video-frames/js/index.js
@@ -0,0 +1,6 @@
+/**
+ * step 1: open file system and grab video
+ * step 2: grab the video track and create a new processor 
+ * step 3: create a new transform stream that will save the video frames
+ * step 4: create class to expose all functions "class" is a group of functions and properties
+ */

00e608cc81082d40925652ff8669fb1ac7b7b70a
larry676789 <152258295+larry676789@users.noreply.github.com>
2023-12-29 04:02:55 +0000

    live share

--- working-with-videos/get-video-frames/js/index.js
+++ working-with-videos/get-video-frames/js/index.js
@@ -4,3 +4,10 @@
  * step 3: create a new transform stream that will save the video frames
  * step 4: create class to expose all functions "class" is a group of functions and properties
  */
+async function getFile() {
+    // Open file picker and destructure the result the first handle
+    const [fileHandle] = await window.showOpenFilePicker();
+    const file = await fileHandle.getFile();
+    return file;
+  }
+  
